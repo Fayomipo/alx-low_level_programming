0b36580abea1971831234ec81f53d17f8f15e9cc
Fayomipo <fayomipo@gmai.com>
2023-07-19 13:51:37 -0700

    update dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - details of the dog
+ * @name: First member of struct dog
+ * @age: Second member of struct dog
+ * @owner: Third member of struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
