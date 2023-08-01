335745f31bf19259df6ad3fde39c66b950017a5b
Fayomipo <fayomipo@gmail.com>
2023-08-01 12:44:30 -0700

    updated the betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * list_len - returns the number of elements in the linked list
+ * @h: pointer to list_t
+ *
+ * Return: number of elements in h
+ */
 
 size_t list_len(const list_t *h)
 {
