959684e1aa8603c8c67a2aa1de9004de3d2a7d2a
Fayomipo <fayomipo@gmail.com>
2023-08-01 13:46:39 -0700

    rectified data error in the node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	size_t addr = 0;
+	unsigned int addr = 0;
 
 	while (str[addr])
 		addr++;
@@ -21,7 +21,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->addr = addr;
+	new->len = addr;
 	new->next = *head;
 	*head = new;
 
